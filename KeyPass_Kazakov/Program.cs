48eafb45304b634e400969dfc64fafc096ba77f2
anekk <aneckalarionova@gmail.com>
2026-03-26 13:43:22 +0500

    Подключение Swagger для сервера.  Казаков

--- KeyPass_Kazakov/Program.cs
+++ KeyPass_Kazakov/Program.cs
@@ -1,6 +1,30 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddMvc(option => option.EnableEndpointRouting = true);
+builder.Services.AddSwaggerGen(option =>
+{
+    option.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Инструментарий"
+    });
+
+    string PathFile = Path.Combine(AppContext.BaseDirectory, "KeePass.xml");
+    option.IncludeXmlComments(PathFile);
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseSwagger();
 
+app.UseRouting();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers(); 
+});
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Инструментарий");
+});
 app.Run();
+
