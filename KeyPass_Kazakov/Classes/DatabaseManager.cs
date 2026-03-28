3fd56bf7b532499a3a9ecfc0b6da196bfe55cd58
anekk <aneckalarionova@gmail.com>
2026-03-28 16:30:39 +0500

    Разработанные вспомогательные классы для сервера. Казаков

---
+++ KeyPass_Kazakov/Classes/DatabaseManager.cs
@@ -0,0 +1,19 @@
+﻿using KeyPass_Kazakov.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace KeyPass_Kazakov.Classes
+{
+    public class DatabaseManager : DbContext
+    {
+        public DbSet<Storage> Storages { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DatabaseManager() =>
+            Database.EnsureCreated();
+
+        public override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql("server=127.0.0.1;uid=root;pwd=;database=Storage;",
+            new MySqlServerVersion(new Version(8, 0, 11)));
+        }
+    }
+}
---
+++ KeyPass_Kazakov/Classes/JwtToken.cs
@@ -0,0 +1,58 @@
+﻿using System.Diagnostics;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace KeyPass_Kazakov.Classes
+{
+    public class JwtToken
+    {
+        static byte[] Key = Encoding.UTF8.GetBytes("PERMAVIAT_THE_BEST!!!!!!!!!");
+
+        public static string Generate(User user)
+        {
+            JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+            SecurityTokenDescriptor tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim("UserId", user.Id.ToString())
+                }),
+
+                Expires = DateTime.UtcNow.AddDays(7),
+
+                SigningCredentials = new SigningCredentials(
+                    new SymmetricSecurityKey(Key),
+                    SecurityAlgorithms.HmacSha256Signature
+                    )
+            };
+            SecurityToken Token = TokenHandler.CreateToken(tokenDescriptor);
+            return TokenHandler.WriteToken(Token);
+        }
+
+        public static int? GetUserIdFromToken(string token)
+        {
+            try
+            {
+                JwtSecurityTokenHandler TokenHandler = new JwtSecurityTokenHandler();
+                TokenHandler.ValidateToken(token, new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ClockSkew = TimeSpan.Zero
+                }, out SecurityToken validatedToken);
+
+                JwtSecurityToken JwtToken = (JwtSecurityToken)validatedToken;
+                string UserId = JwtToken.Claims.First(x => x.Type == "UserId").Value;
+                return int.Parse(UserId);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
